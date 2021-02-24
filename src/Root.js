21214650140b5a9f31c18106abf596af8d35a002
Pierre Goutheraud <pierre.goutheraud@gmail.com>
2021-02-24 19:24:56 +0100

    Add react-router and create Home

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import store from "./redux/store";
+import Home from "./pages/Home/Home";
 
 export default function Root() {
-  return <Provider store={store}></Provider>;
+  return (
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
+  );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+export default function Home() {
+  return <main className={styles.container}>Home</main>;
+}
