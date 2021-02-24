1bcadba5a5c6f33bf170139986b30a58f527c8e4
Pierre Goutheraud <pierre.goutheraud@gmail.com>
2021-02-24 21:38:30 +0100

    Style login page with Traktor

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Tractor, Spacer, Typography } from "@aircall/tractor";
 import store from "./redux/store";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import Home from "./pages/Home/Home";
@@ -8,17 +9,19 @@ import Login from "./pages/Login/Login";
 
 export default function Root() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <PrivateRoute path="/">
-            <Home />
-          </PrivateRoute>
-        </Switch>
-      </Router>
-    </Provider>
+    <Tractor injectStyle>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <PrivateRoute path="/">
+              <Home />
+            </PrivateRoute>
+          </Switch>
+        </Router>
+      </Provider>
+    </Tractor>
   );
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import {
+  Box,
+  Form,
+  Grid,
+  FormItem,
+  TextFieldInput,
+  Button,
+} from "@aircall/tractor";
 import styles from "./Login.module.css";
+
+/*
+This is not a fancy login page (No error report, no loading etc.)
+but I did not want to spend too much time on this part of the app.
+Also in the documentation of Traktor there is a onSubmit prop on the form, but I
+tried using it and it was not called. I am sure you have a better way to get
+the data from the form but meanwhile I did a very simple solution with onChange and onClick props.
+*/
 
 export default function Login() {
   const auth = useAuth();
   let history = useHistory();
   let location = useLocation();
   let { from } = location.state || { from: { pathname: "/" } };
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  async function handleLogin() {
+  async function handleSubmit() {
+    if (!username.length || !password.length) {
+      return;
+    }
+
     try {
-      await auth.login("pierre", "123");
+      await auth.login(username, password);
       history.replace(from);
     } catch (e) {
       return console.log(e);
@@ -20,7 +42,28 @@ export default function Login() {
 
   return (
     <main className={styles.container}>
-      <button onClick={handleLogin}>Login</button>
+      <Box width="400px">
+        <Form>
+          <Grid gridColumnGap={4} gridRowGap={5} gridTemplateColumns="1fr">
+            <FormItem label="Username" name="username">
+              <TextFieldInput
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+            </FormItem>
+            <FormItem label="Password" name="email">
+              <TextFieldInput
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </FormItem>
+            <FormItem onClick={handleSubmit}>
+              <Button block>Login</Button>
+            </FormItem>
+          </Grid>
+        </Form>
+      </Box>
     </main>
   );
 }
