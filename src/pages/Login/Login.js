f573a1a6d020d1e3c3d582e7f502b474b8d245bf
Pierre Goutheraud <pierre.goutheraud@gmail.com>
2021-02-25 16:16:53 +0100

    Login comments

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import {
@@ -12,11 +12,9 @@ import {
 import styles from "./Login.module.css";
 
 /*
-This is not a fancy login page (No error report, no loading etc.)
-but I did not want to spend too much time on this part of the app.
-Also in the documentation of Traktor there is a onSubmit prop on the form, but I
-tried using it and it was not called. I am sure you have a better way to get
-the data from the form but meanwhile I did a very simple solution with onChange and onClick props.
+This is not a fancy login page (No error report, no loading etc.) but I did not want to spend too much time on this part of the app.
+Also in the documentation of Traktor there is a onSubmit prop on the form, but I tried using it and it was not called.
+I am sure you have a better way to get the data from the form but I could not find the if so I did a very simple solution with onChange and onClick props.
 */
 
 export default function Login() {
@@ -26,6 +24,13 @@ export default function Login() {
   let { from } = location.state || { from: { pathname: "/" } };
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  // TEST PURPOSE ! REMOVE THIS !
+  useEffect(() => {
+    auth.login("pierre", "123").then(() => {
+      history.replace("/");
+    });
+  }, []);
 
   async function handleSubmit() {
     if (!username.length || !password.length) {
